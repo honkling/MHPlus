d23b343cb9b90d7582e999ab80f40e014a1e8901
AppleFlavored <36705595+AppleFlavored@users.noreply.github.com>
2020-06-08 15:13:29 -0400

    Add common.css + clean up inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,10 @@
-// TODO: Add support for multiple themes
+function loadStyle(url) {
+    const style = document.createElement('link');
+    style.href = chrome.extension.getURL(`css/${url}`);
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    document.head.appendChild(style);
+}
 
-const style = document.createElement('link');
-style.href = chrome.extension.getURL('css/purple.css');
-style.type = 'text/css';
-style.rel = 'stylesheet';
-document.head.appendChild(style);
+loadStyle('common.css');
+loadStyle('purple.css');
